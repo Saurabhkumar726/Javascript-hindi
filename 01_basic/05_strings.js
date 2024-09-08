c59a043849f016ad1c0543181c7e2962da978648
Saurabh Kumar <81978956+Saurabhkumar726@users.noreply.github.com>
2024-09-08 04:04:10 +0000

    Initial commit

---
+++ 01_basic/05_strings.js
@@ -0,0 +1,33 @@
+const name = "sau-rabh"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('saurabh')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('s'));
+
+const newString = gameName.substring(-8,4)
+console.log(newString);
+
+const anotherString = gameName.slice(-6, 4)
+console.log(anotherString);
+
+const newStringOne = ' saurabh '
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://saurabh.com/saurabh%20kumar"
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('sundar'))
+console.log(gameName.split('-'));
